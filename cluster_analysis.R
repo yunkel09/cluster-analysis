99b0e2fd81c0926bc5163746fd97f286eaa2e8f9
William Chavarria <wchavarria@tigo.com.gt>
2017-09-07 06:34:59 -0600

    agregue dendograma

--- cluster_analysis.R
+++ cluster_analysis.R
@@ -79,11 +79,47 @@
                bg  = colores[fallas_scaled$zona],
                pch = 21, hist.col = 'gold1')
   
-  # notas: observamos una alta correlacion entre
+  # notas: observamos una alta correlacion entre bandejas e hilos, seguido de
+  # fibra y mufas.  
   
+  
+  
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### SAMPLING                                                                ####
+
+  # tomar una muestra
+  set.seed(1234)
+  
+  new <- sample(1:dim(fallas_scaled)[1], 15)
+  
+  ds <- fallas_scaled[new, ]
   
   # distance matrix
-  distance <- dist(fallas_scaled[-1])
+  distance <- dist(ds[-1])
   print(distance, digits = 2)  
   
   
+  ## aqui vemos la distancia entre la observacion 94 y la 80.  La observacion
+  ## 75 y 76 tienen distancia 0.  El valor por defecto de la funcion 'dist' es
+  ## distancia Euclideana.  El objetivo del analisis de conglomerados es agrup-
+  ## par aquellas observaciones que tengan una menor distancia entre ellas.
+  
+  
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### DENDOGRAMA                                                              ####
+
+  hc <- hclust(distance)
+  plot(hc)  
+  
+  #' Analisis: En el eje Y podemos ver la altura que corresponde a la distancia
+  #' entre los puntos.  Cuando dos observaciones estan cercas la una de la otra,
+  #' forman un cluster.  Por ejemplo podriamos trazar una linea horizontal en
+  #' 0.5 y decir que todas las observaciones debajo de esa linea tienen una 
+  #' distancia entre ellas, menor a 0.5.
+  
+ hcd <- as.dendrogram(hc)
+ nodePar <- list(lab.cex = 0.6, pch = c(NA, 19), 
+                 cex = 0.7, col = "blue")
+ plot(hcd, type = 'rectangle', xlab = 'Height', nodePar = nodePar)
+ abline(h = 0.5, lty = 2)  
+ 
